c75fe8c27e10d900cb8dc4e46ae1444fc9700be7
Anurag <shawanurag2012@gmail.com>
2024-03-21 01:31:47 +0530

    videos added for prduction

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
-# Vikram
+# React + Vite
 
-Vikram is a guide for my college JBIT that guides you to different locations according to your choice.
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-This is a MERN PROJECT.
+Currently, two official plugins are available:
 
-The path is shown with the help of images that is stored in Google drive. The links of all the images is stored in mongo db that has location and steps field. The location has the goal address and steps is an array of urls of the images.
-
-The backend fetches images from Google drive using node-fetch and sends it to the frontend.
-
-The backend of Vikram is in Backend-vikram repository.
-It has 3 endpoints. 
-1) To store image links to mongo.
-2) To get the array size of the steps for the location.
-3) To fetch images from Google drive and send it to frontend.
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/Components/LocationPlayer.jsx
+++ src/Components/LocationPlayer.jsx
@@ -5,9 +5,12 @@ import vikram from '../assets/image-vikram.png'
 import ImageViewer from './ImageViewer'
 import { useNavigate } from 'react-router-dom'
 import Header from './Header'
+import { choosenLanguage } from '../store/atoms/Language'
 
 function LocationPlayer() {
   const navigate = useNavigate();
+  const lang = useRecoilValue(choosenLanguage);
+
   const choosenLocation = useRecoilValue(Locationatom)
   console.log(choosenLocation)
   return (
@@ -17,6 +20,9 @@ function LocationPlayer() {
 
       <ImageViewer />
       <img src={vikram} alt="" className='lg:w-1/5 absolute lg:bottom-[5%]  lg:left-[15%]  lg:rotate-0 rounded-bl-xl bottom-[2%] w-[50%] rotate-90'  />
+      
+        <div className='font-bold text-xl rotate-90 absolute bottom-[50%] right-[0%] translate-x-[20%] z-10 bg-slate-100 px-4 py-2 bg-opacity-70 rounded-2xl'>{lang=='En'? "Use buttons to navigate":"नेविगेट करने के लिए बटनों का उपयोग करें"}</div>
+        
       <div className="homeContainer h-20 w-20 border-2 border-white rounded-full hover:cursor-pointer  absolute right-[2%] top-[2%]">
         <i className="fa-solid fa-house text-5xl text-white p-3" onClick={() => navigate('/')}></i>
       </div>
