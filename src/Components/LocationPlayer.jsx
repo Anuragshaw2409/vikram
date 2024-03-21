ee2e02692102b85fe37c5c6a11f40ec01be1ca28
Anurag <shawanurag2012@gmail.com>
2024-03-21 23:14:02 +0530

    aSas

--- src/Components/LocationPlayer.jsx
+++ src/Components/LocationPlayer.jsx
@@ -21,7 +21,7 @@ function LocationPlayer() {
       <ImageViewer />
       <img src={vikram} alt="" className='lg:w-1/5 absolute lg:bottom-[5%]  lg:left-[15%]  lg:rotate-0 rounded-bl-xl bottom-[2%] w-[50%] rotate-90'  />
       
-        <div className='font-bold text-xl rotate-90 absolute bottom-[50%] right-[0%] translate-x-[20%] z-10 bg-slate-100 px-4 py-2 bg-opacity-70 rounded-2xl z-20'>{lang=='En'? "Use buttons to navigate":"नेविगेट करने के लिए बटनों का उपयोग करें"}</div>
+        <div className='font-bold text-xl rotate-90 absolute bottom-[50%] right-[0%] translate-x-[20%]  bg-slate-100 px-4 py-2 bg-opacity-70 rounded-2xl z-20'>{lang=='En'? "Use buttons to navigate":"नेविगेट करने के लिए बटनों का उपयोग करें"}</div>
         
       <div className="homeContainer h-20 w-20 border-2 border-white rounded-full hover:cursor-pointer  absolute right-[2%] top-[2%]">
         <i className="fa-solid fa-house text-5xl text-white p-3" onClick={() => navigate('/')}></i>
